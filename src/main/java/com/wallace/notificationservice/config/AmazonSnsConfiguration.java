f7478763145136f1119e42b926a00c564fbf97e5
henriquewallace <henrique.wallace@townsq.com.br>
2024-06-17 20:19:42 -0300

    feat: create method to authentic on AWS and build an instance of AmazonSNS

--- src/main/java/com/wallace/notificationservice/config/AmazonSnsConfiguration.java
+++ src/main/java/com/wallace/notificationservice/config/AmazonSnsConfiguration.java
@@ -1,6 +1,13 @@
 package com.wallace.notificationservice.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,4 +18,16 @@ public class AmazonSnsConfiguration {
 
     @Value("${aws.secretKey}")
     private String secretKey;
+
+    @Bean
+    public AWSCredentials awsCredentials() {
+        return new BasicAWSCredentials(accessKey, secretKey);
+    }
+
+    @Bean
+    public AmazonSNS amazonSNS() {
+        return AmazonSNSClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials()))
+                .withRegion(Regions.US_EAST_1).build();
+    }
 }
