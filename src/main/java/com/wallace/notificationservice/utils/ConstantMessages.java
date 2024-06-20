6f0cf12e70e2f80b8ce4c2c8d2a6cf9e49ed33b5
henriquewallace <henrique.wallace@hotmail.com>
2024-06-20 20:36:01 -0300

    fix: remove unnecessary dot after reproved reason

--- src/main/java/com/wallace/notificationservice/utils/ConstantMessages.java
+++ src/main/java/com/wallace/notificationservice/utils/ConstantMessages.java
@@ -4,5 +4,5 @@ public class ConstantMessages {
 
     public static final String PROPOSAL_UNDER_ANALYSIS = "Dear %s, your proposal was received by our team and will be analyzed. We'll get back in touch soon.";
     public static final String APPROVED_PROPOSAL = "Congratulations, %s! Your credit analysis solicitation was attended. Approved credit in the amount of %s.";
-    public static final String REPROVED_PROPOSAL = "Dear %s, unfortunately your credit analysis solicitation was not attended. Reason: %s.";
+    public static final String REPROVED_PROPOSAL = "Dear %s, unfortunately your credit analysis solicitation was not attended. Reason: %s";
 }
